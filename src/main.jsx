20271501bd77217fa9fa279105cb46260c8be2e3
JesusBlan <jezublanco@gmail.com>
2025-03-03 21:54:59 +0100

    cleaned and formated a bit

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import "./index.css";
 import Header from "./modules/header";
 import Info from "./modules/Info";
 
+
 createRoot(document.getElementById("root")).render(
   <>
     <Header />
     <Info />
+
+    
   </>,
 );
