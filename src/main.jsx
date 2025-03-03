177ba5ec46fda81babc87008267acb89f4f898ad
JesusBlan <jezublanco@gmail.com>
2025-03-03 13:41:28 +0100

    added a header with some svgs and formated all with prettier

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
+
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import Header from './modules/header'
+
 
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <>
   
-  </StrictMode>,
+    <Header />
+  </>
 )
