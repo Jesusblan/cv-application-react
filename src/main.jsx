b45d06038e9201a43916144546d303b9a3e54ab5
JesusBlan <jezublanco@gmail.com>
2025-03-05 12:00:46 +0100

    reestructures modules to get it all inside info module

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,14 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import Header from "./modules/header";
-import Info from "./modules/Info";
-import SetPopup from "./modules/EditData";
+
+import Info from "./modules/EditData";
 import infoData from "./modules/data";
 
 createRoot(document.getElementById("root")).render(
   <>
     <Header />
     <Info props={infoData}/>
-    <SetPopup />
   </>,
   
 );
--- src/modules/Info.jsx
+++ src/modules/Info.jsx
@@ -1,4 +1,63 @@
-import "../styles/Info.css";
+import "../styles/EditData.css";
+import React, { useState } from "react";
+import infoData from "./data";
+
+function SetPopup() {
+  const [isPopup, setPopup] = useState(false);
+
+  if (isPopup === false) {
+    return (
+      <button className="edit" onClick={() => setPopup(true)}>
+        Edit Info
+      </button>
+    );
+  } else {
+    return (
+      <div className="input">
+        <InfoForm />
+
+        <button className="close" onClick={() => setPopup(false)}>
+          Close
+        </button>
+      </div>
+    );
+  }
+}
+
+function InfoForm() {
+  const [isData, setData] = useState(infoData)
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    infoData.name = event.target.inputName.value;
+    infoData.lastName = event.target.inputLastName.value;
+    infoData.mail = event.target.inputMail.value;
+    infoData.phone = event.target.inputPhone.value;
+    setData(infoData)
+
+    console.log(isData)
+  };
+
+  return (
+    <div>
+      <form className="infoForm" onSubmit={handleSubmit}>
+        <label>Name: </label>
+        <input name="inputName" type="text" />
+
+        <label>Last Name: </label>
+        <input name="inputLastName" type="text" />
+
+        <label>Mail: </label>
+        <input name="inputMail" type="email" />
+
+        <label>Phone: </label>
+        <input name="inputPhone" type="tel" />
+
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
 
 
 
@@ -10,8 +69,12 @@ function Info({props}) {
       <p>Last Name: {props.lastName}</p>
       <p>Mail: {props.mail}</p>
       <p>Phone: {props.phone}</p>
+      <SetPopup />
     </div>
   );
 }
+
+
+
 
 export default Info;
