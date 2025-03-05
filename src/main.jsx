9e7606eed8202b5984c6528b60ede89e9f5919db
JesusBlan <jezublanco@gmail.com>
2025-03-05 13:35:00 +0100

    final touchs in all files

--- src/main.jsx
+++ src/main.jsx
@@ -10,8 +10,8 @@ createRoot(document.getElementById("root")).render(
     <Header />
     <div className="display">
       <Info />
-      <Education />
       <Experience />
+      <Education />
     </div>
   </>,
 );
--- src/modules/Experience.jsx
+++ src/modules/Experience.jsx
@@ -3,9 +3,14 @@ import React, { useState } from "react";
 
 function Experience() {
   let expData = {
-    position: "Junior Web Dev",
+    position: "Senior Web Dev",
     startEnd: "05/2023-Actuality",
     enterprise: "Odin Project",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    position2: "junior Web Dev",
+    startEnd2: "09/2021-05/2023",
+    enterprise2: "Odin Project",
+    description2: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
 
   };
 
@@ -16,6 +21,11 @@ function Experience() {
     expData.position = event.target.inputPosition.value;
     expData.startEnd = event.target.inputStartEnd.value;
     expData.enterprise = event.target.inputEnterprise.value;
+    expData.description = event.target.inputDescription.value;
+    expData.position2 = event.target.inputPosition2.value;
+    expData.startEnd2 = event.target.inputStartEnd2.value;
+    expData.enterprise2 = event.target.inputEnterprise2.value;
+    expData.description2 = event.target.inputDescription2.value
 
     setData(expData);
 
@@ -37,7 +47,22 @@ function Experience() {
             <label>Enterprise: </label>
             <input name="inputCenter" type="text" />
 
-            <button type="submit">Submit</button>
+            <label>Description: </label>
+            <input name="inputDescription" type="text" />
+            
+            <label>Position:  </label>
+            <input name="inputTitle2" type="text" />
+
+            <label>Date: </label>
+            <input name="inputStartEnd2" type="text" />
+
+            <label>Enterprise: </label>
+            <input name="inputCenter2" type="text" />
+
+            <label>Description: </label>
+            <input name="inputDescription2" type="text" />
+
+            <button type="submit2">Submit</button>
           </form>
         </div>
       );
@@ -70,6 +95,12 @@ function Experience() {
       <p>Position: {isData.position}</p>
       <p>Date: {isData.startEnd}</p>
       <p>Enterprise: {isData.enterprise}</p>
+      <p>Description: {isData.description}</p>
+      <br />
+      <p>Position: {isData.position2}</p>
+      <p>Date: {isData.startEnd2}</p>
+      <p>Enterprise: {isData.enterprise2}</p>
+      <p>Description: {isData.description2}</p>
       <SetPopup />
     </div>
   );
